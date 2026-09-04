974d7e13f2b3acdfd2cffcae8a7a70d98c27f6cb
oxshipon1@gmail.com <oxshipon1@gmail.com>
2026-09-04 17:12:13 +0600

    fix error

--- src/components/ThemeScript.tsx
+++ src/components/ThemeScript.tsx
@@ -18,6 +18,44 @@ export default function ThemeScript() {
                   document.documentElement.classList.remove('dark');
                 }
               } catch (e) {}
+
+              try {
+                if (typeof MutationObserver !== 'undefined') {
+                  var clean = function(node) {
+                    if (node && node.nodeType === 1) {
+                      if (node.hasAttribute('bis_skin_checked')) {
+                        node.removeAttribute('bis_skin_checked');
+                      }
+                      if (node.querySelectorAll) {
+                        var list = node.querySelectorAll('[bis_skin_checked]');
+                        for (var i = 0; i < list.length; i++) {
+                          list[i].removeAttribute('bis_skin_checked');
+                        }
+                      }
+                    }
+                  };
+
+                  var observer = new MutationObserver(function(mutations) {
+                    for (var i = 0; i < mutations.length; i++) {
+                      var m = mutations[i];
+                      if (m.type === 'attributes' && m.attributeName === 'bis_skin_checked') {
+                        m.target.removeAttribute('bis_skin_checked');
+                      } else if (m.type === 'childList') {
+                        for (var j = 0; j < m.addedNodes.length; j++) {
+                          clean(m.addedNodes[j]);
+                        }
+                      }
+                    }
+                  });
+
+                  observer.observe(document.documentElement, {
+                    attributes: true,
+                    attributeFilter: ['bis_skin_checked'],
+                    childList: true,
+                    subtree: true
+                  });
+                }
+              } catch (e) {}
             })()
           `,
         }}
@@ -27,3 +65,4 @@ export default function ThemeScript() {
 
   return null;
 }
+
